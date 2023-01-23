176ed6bb43270f6a6558ece17c9cec224b53c8de
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-23 15:15:34 +0530

    ADDED : Code To Set Event Listener When Documents Is Loaded

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,4 +1,4 @@
-// UC 4
+// Day-43_UC 4
 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
@@ -7,11 +7,37 @@ salary.addEventListener('input', function() {
     output.textContent = salary.value;
 });
 
-// UC 7
+// Day_43_UC 7
 const text = document.querySelector('#name');
-const textError = document.querySelector('.name-error');
+const textError = document.querySelector('.text-error');
 text.addEventListener('input', function() {
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
     if(nameRegex.test(text.value)) textError.textContent = "";
     else textError.textContent = "Name is Incorrect"
+});
+
+// Day_44_UC 2
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch(e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
 });
