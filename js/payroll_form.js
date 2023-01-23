abc440b8c0525f665eeeea1db6eb1842adcc54a2
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-23 14:40:56 +0530

    ADDED : Code To Validate UserName And StartDate

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,6 +1,17 @@
+// UC 4
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
+});
+
+// UC 7
+const text = document.querySelector('#name');
+const textError = document.querySelector('.name-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value)) textError.textContent = "";
+    else textError.textContent = "Name is Incorrect"
 });
