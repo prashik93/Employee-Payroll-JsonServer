7eace844701d7e59c0a52eb8e8ce0c01e302a0cb
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-23 23:57:05 +0530

    ADDED : Code To View Employee Payroll Details In A Tabular Format From JSON Object

--- js/home.js
+++ js/home.js
@@ -6,24 +6,65 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-    <tr>
-        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse 1.jpg"></td>
-        <td>Prashik Kamble</td>
-        <td>Male</td>
-        <td>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div>
-        </td>
-        <td>3000000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img id="1" class="edit-delete" onclick="remove(this)" alt="delete"
-                src="../assets/icons/delete-black-18dp.png">
-            <img id="1" class="edit-delete" alt="edit" onclick="update(this)"
-                src="../assets/icons/create-black-18dp.png">
-        </td>
-    </tr>
-    `;
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name="${empPayrollData._id}" class="edit-delete" onclick="remove(this)" alt="delete"
+                    src="../assets/icons/delete-black-18dp.png">
+                <img name="${empPayrollData._id}" class="edit-delete" alt="edit" onclick="update(this)"
+                    src="../assets/icons/create-black-18dp.png">
+            </td>
+        </tr>
+        `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: 'Prashik Kamble',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '23 Jan 2022',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -7.jpg'
+        },
+        {
+            _name: 'Pratik Kamble',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Sales'
+            ],
+            _salary: '700000',
+            _startDate: '21 Jan 2022',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/profile-images/Ellipse 1.jpg'
+        }
+    ];
+    return employeePayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml
 }
